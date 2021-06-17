230469e5334ec6060457ee84b1a5039a6f40a09d
Vanysha69 <86082676+Vanysha69@users.noreply.github.com>
2021-06-18 01:00:32 +0300

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\ponderoussilence-Web-Deploy.pubxml -p:Password=Ff4DNLSair0X9KHDlfD3cN87ttxSHkoyQLzqkM8Q4u6iNW5pP21yYlEgxLGH
+
